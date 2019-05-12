9427b6b35cf659be59b9d8d5aa2bfeb0b3d933dd
James Lissiak <blanksourcecode@gmail.com>
2019-05-12 12:58:00 -0700

    Adding helpful build scripts

---
+++ keyboards/keebio/iris/keymaps/blanksourcecode/build.bat
@@ -0,0 +1,1 @@
+D:\msys64\usr\bin\mintty.exe /usr/bin/env /usr/bin/bash -l -c /d/Git/qmk_firmware/keyboards/keebio/iris/keymaps/blanksourcecode/build.sh
---
+++ keyboards/keebio/iris/keymaps/blanksourcecode/build.sh
@@ -0,0 +1,3 @@
+source /d/msys64/home/jalis/qmk_utils/activate_msys2.sh
+cd /d/Git/qmk_firmware/
+make keebio/iris/rev2:blanksourcecode
